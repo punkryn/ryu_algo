89db6fbd11d091141b65d90a278cb935755a9ac4
punkryn <gns8167@naver.com>
2022-01-24 13:48:01 +0900

    dp

---
+++ 동적프로그래밍/계단수(비트마스크).py
@@ -0,0 +1,31 @@
+import sys
+
+def main():
+    n = int(sys.stdin.readline())
+    MOD = 1000000000
+    dp = [[[0] * (1 << 10) for _ in range(10)] for _ in range(n + 1)]
+
+    for i in range(1, 10):
+        dp[1][i][1 << i] = 1
+
+    FULL = (1 << 10) - 1
+
+    for length in range(2, n + 1):
+        for i in range(10):
+            for j in range(FULL + 1):
+                if i == 0:
+                    dp[length][i][j | 1 << i] = (dp[length][i][j | 1 << i] + dp[length-1][1][j]) % MOD
+                elif i == 9:
+                    dp[length][i][j | 1 << i] = (dp[length][i][j | 1 << i] + dp[length-1][8][j]) % MOD
+                else:
+                    dp[length][i][j | 1 << i] = (dp[length][i][j | 1 << i] + dp[length-1][i-1][j]) % MOD
+                    dp[length][i][j | 1 << i] = (dp[length][i][j | 1 << i] + dp[length-1][i+1][j]) % MOD
+
+    ans = 0
+    for i in range(10):
+        ans = (ans + dp[n][i][FULL]) % MOD
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
--- 동적프로그래밍/계단수.py
+++ 동적프로그래밍/계단수.py
@@ -1,2 +1,29 @@
 # https://acmicpc.net/problem/1562
 import sys
+
+def main():
+    n = int(sys.stdin.readline())
+    dp = [[[[0 for _4 in range(10)] for _3 in range(10)] for _2 in range(10)] for _1 in range(n + 1)]
+    MOD = 1000000000
+
+    for num in range(1, 10):
+        dp[1][num][num][num] = 1
+
+    for len in range(2, n + 1):
+        for prev in range(10):
+            for low in range(10):
+                for high in range(10):
+                    for cur in [prev - 1, prev + 1]:
+                        if cur < 0 or cur > 9: continue
+                        dp[len][cur][min(low, cur)][max(high, cur)] += dp[len - 1][prev][low][high]
+                        dp[len][cur][min(low, cur)][max(high, cur)] %= MOD
+
+    ans = 0
+    for i in range(10):
+        ans += dp[n][i][0][9]
+        ans %= MOD
+
+    print(ans)
+
+if __name__ == "__main__":
+    main()
