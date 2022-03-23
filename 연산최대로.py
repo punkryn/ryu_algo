e8f614ccb05cce3e3c47c71668483e2450ab2602
punkryn <gns816715@gmail.com>
2022-03-23 15:25:56 +0900

    '0323'

---
+++ 연산최대로.py
@@ -0,0 +1,49 @@
+# https://www.acmicpc.net/problem/21943
+import sys
+from itertools import permutations as pt
+si = sys.stdin.readline
+
+def main():
+    n = int(si())
+    x = list(map(int, si().split()))
+    p, q = map(int, si().split())
+
+    if q == 0:
+        print(sum(x))
+        return
+    elif p == 0:
+        ans = 1
+        for v in x:
+            ans *= v
+        print(ans)
+        return
+
+    candidate = []
+    def go(depth, total, cur, prev):
+        if depth == 0:
+            if total == n:
+                candidate.append(tuple(cur))
+            return
+        
+        for i in range(prev, n - depth + 1):
+            if i == 0: continue
+            cur.append(i)
+            go(depth - 1, total + i, cur, i)
+            cur.pop()
+    
+    go(q + 1, 0, [], 0)
+
+
+    ans = 0
+    for per in pt(x, n):
+        for cand in candidate:
+            start = 0
+            total = 1
+            for c in cand:
+                total *= sum(per[start:start + c])
+                start += c
+            ans = max(ans, total)
+    print(ans)
+
+if __name__ == '__main__':
+    main()
---
+++ 탑보기.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/22866
+import sys
+si = sys.stdin.readline
+
+def main():
+    n = int(si())
+    heights = list(map(int, si().split()))
+    
