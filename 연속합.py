faa9d508171fdbead738b16269cddbb3b7d5b529
punkryn <gns816715@gmail.com>
2022-04-05 22:55:18 +0900

    0405

--- 연속합.py
+++ 연속합.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     dp = [[-1001] * 2 for _ in range(n)]
     dp[0][0] = a[0]
     for i in range(1, n):
-        dp[i][0] = max(dp[i - 1][0] + a[i], a[i])
+        # i번째를 선택하는데 연속하는 경우 vs 연속하지 않는 경우
+        dp[i][0] = max(dp[i - 1][0] + a[i], a[i]) 
+        # i번째를 선택 안 하는 경우
         dp[i][1] = max(dp[i - 1])
     print(max(dp[n-1]))
