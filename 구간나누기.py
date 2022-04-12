3113212b91636c574f2d43a00849cef77d1081a9
punkryn <gns8167@naver.com>
2022-04-12 16:28:52 +0900

    '0412'

--- 구간나누기.py
+++ 구간나누기.py
@@ -6,16 +6,9 @@ if __name__ == "__main__":
     n, m = map(int, si().split())
     a = [int(si()) for _ in range(n)]
     
-    dp = [[-INF] * n for _ in range(m)]
-    dp[0][0] = a[0]
-    start, end = 0, 0
-    for i in range(m):
-
-        for j in range(i * 2, i * 2 + n - 2 ** (m - 1)):
-            if i == 0 and j == 0: continue
-            if i == 0:
-                dp[i][j] = max(dp[i][j - 1] + a[j], a[j])
-            else:
-                dp[i][j] = max(dp[i - 1][j - 2] + a[j], dp[i][j - 1] + a[j])
-                
-    print(max(dp[-1]))
+    dp = [[-INF] * 2 for _ in range(n + 1) for _ in range(m + 1)]
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            dp[i][j][0] = max(dp[i - 1][j])
+            dp[i][j][1] = max(dp[i - 1][j - 1])
