43064a1beeb7773da9bdad8b6b81b92cef7e5517
punkryn <gns8167@naver.com>
2022-05-13 21:52:17 +0900

    '0513'

--- 독서실거리두기.py
+++ 독서실거리두기.py
@@ -2,14 +2,48 @@
 from sys import stdin
 si = stdin.readline
 
+def dnq(l, r):
+    global num
+    if l == r - 1:
+        return
+    
+    mid = (l + r) // 2
+    seat[num] = mid
+    num += 1
+    dnq(l, mid)
+    dnq(mid, r)
+
 if __name__ == '__main__':
     n, t, p = map(int, si().split())
-    timestamp = [0] * (60 * 21 + 1)
+    timestamp = [[-1] * (60 * 21 + 1) for _ in range(n + 1)]
     order = []
     for _ in range(t):
         start, end = si().split()
         start = int(start[:2]) * 60 + int(start[2:])
-        end = int(end[:2]) * 60 + int(end[:2])
+        end = int(end[:2]) * 60 + int(end[2:])
         order.append((start, end, end - start))
     order.sort(key=lambda x: (x[0], x[2]))
     
+    seat = [0] * (n + 1)
+    seat[1] = 1
+    if n >= 2:
+        seat[2] = n
+    if n >= 3:
+        num = 3
+        dnq(1, n)
+    for i in range(len(order)):
+        s, e, _ = order[i]
+        cnt = 1
+        for j in range(i):
+            s_, e_, _ = order[j]
+            if s < e_:
+                cnt += 1
+        for j in range(s, e):
+            timestamp[seat[cnt]][j] = 1
+    
+    ans = 0
+    for i in range(9 * 60, 21 * 60):
+        if timestamp[p][i] == -1:
+            ans += 1
+
+    print(ans)
--- 후보추천하기.py
+++ 후보추천하기.py
@@ -7,4 +7,13 @@ if __name__ == '__main__':
     total = int(si())
     number = list(map(int, si().split()))
 
-    
+    t = dict()
+    for idx, num in enumerate(number):
+        if num in t:
+            t[num][0] += 1
+        else:
+            if len(t) == n:
+                minv = min(t.items(), key=lambda x: (x[1][0], x[1][1]))[0]
+                del t[minv]
+            t[num] = [1, idx]
+    print(*sorted(t.keys()))
